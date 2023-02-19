3632ecf63f0f2624c8f4acae2b01e9308759d627
Roman <romakorolev9702@gmail.com>
2023-02-19 17:41:15 +0300

    new

--- app/src/main/java/android/compose/MainActivity.kt
+++ app/src/main/java/android/compose/MainActivity.kt
@@ -11,8 +11,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import android.compose.ui.theme.ComposeTheme
 import android.text.style.BackgroundColorSpan
+import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -20,9 +23,12 @@ import androidx.compose.material.Card
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 import org.intellij.lang.annotations.PrintFormat
 
@@ -33,6 +39,7 @@ class MainActivity : ComponentActivity() {
             Column() {
                 Cap()
                 ListItem(name = "Роман Королёв", number = "+79541024498")
+                profButton("Моё Резюме")
 
             }
         }
@@ -40,7 +47,6 @@ class MainActivity : ComponentActivity() {
 }
 
 
-@Preview(showBackground = true)
 @Composable
 private fun Cap() {
 
@@ -58,14 +64,22 @@ private fun Cap() {
                 painter = painterResource(id = R.drawable.bell),
                 contentDescription = "bell",
                 contentScale = ContentScale.Inside,
-                modifier = Modifier.size(40.dp)
+                modifier = Modifier
+                    .size(40.dp)
+                    .clickable {
+
+                    }
             )
             Text(text = "Профиль")
             Image(
                 painter = painterResource(id = R.drawable.tool),
                 contentDescription = "tool",
                 contentScale = ContentScale.Inside,
-                modifier = Modifier.size(40.dp)
+                modifier = Modifier
+                    .size(40.dp)
+                    .clickable {
+
+                    }
             )
 
         }
@@ -79,12 +93,16 @@ private fun Cap() {
 private fun ListItem(name: String, number: String) {
     Card(
         modifier = Modifier
+            .background(Color.White)
             .fillMaxWidth()
+            .fillMaxSize(0.2f)
             .padding(10.dp),
         elevation = 5.dp,
         shape = RoundedCornerShape(15.dp)
     )
     {
+
+        
         Box() {
             Row(verticalAlignment = Alignment.CenterVertically) {
                 Image(
@@ -93,12 +111,12 @@ private fun ListItem(name: String, number: String) {
                     contentScale = ContentScale.Crop,
                     modifier = Modifier
                         .padding(5.dp)
-                        .size(64.dp)
+                        .size(100.dp)
                         .clip(CircleShape)
                 )
                 Column() {
-                    Text(text = name)
-                    Text(text = number)
+                    Text(text = name, fontSize = 25.sp)
+                    Text(text = number, fontSize = 17.sp)
 
                 }
             }
@@ -106,6 +124,29 @@ private fun ListItem(name: String, number: String) {
 
     }
 }
+
+@Preview
+@Composable
+private fun profButton(context: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.13f)
+            .padding(10.dp),
+        elevation = 5.dp,
+        shape = RoundedCornerShape(15.dp),
+    ) {
+        Box(modifier = Modifier.background(Color.Black),
+            contentAlignment = Alignment.Center){
+            Text(text = context, color=Color.White)
+        }
+
+
+    }
+
+
+}
+
 
 
 
